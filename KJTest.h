16367d39ad5730321b15c6614479b6248ff0c45c
Koby Jordan <kobyjordan@Kobys-Mac.local>
2014-05-02 16:15:54 +1000

    added test class

---
+++ KJTest.h
@@ -0,0 +1,13 @@
+//
+//  KJTest.h
+//  Git Test
+//
+//  Created by Koby Jordan on 2/05/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KJTest : NSObject
+
+@end
---
+++ KJTest.m
@@ -0,0 +1,13 @@
+//
+//  KJTest.m
+//  Git Test
+//
+//  Created by Koby Jordan on 2/05/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import "KJTest.h"
+
+@implementation KJTest
+
+@end
