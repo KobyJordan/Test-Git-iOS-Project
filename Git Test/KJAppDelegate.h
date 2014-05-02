3240ed63ff7ec73e5253c8ddc84e8729417bff78
Koby Jordan <kobyjordan@Kobys-Mac.local>
2014-05-02 15:51:46 +1000

    Initial Commit

---
+++ Git Test/KJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KJAppDelegate.h
+//  Git Test
+//
+//  Created by Koby Jordan on 2/05/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/KJViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJViewController.h
+//  Git Test
+//
+//  Created by Koby Jordan on 2/05/2014.
+//  Copyright (c) 2014 Koby Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJViewController : UIViewController
+
+@end
